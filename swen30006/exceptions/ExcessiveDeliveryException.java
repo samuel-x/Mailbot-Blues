84c15c316a98177ac80e2720446d20b729eebbb7
amterp <alexander.terp@gmail.com>
2018-04-29 17:38:27 +1000

    Fix

--- swen30006/exceptions/ExcessiveDeliveryException.java
+++ swen30006/exceptions/ExcessiveDeliveryException.java
@@ -4,6 +4,11 @@ package exceptions;
  * An exception thrown when the robot tries to deliver more items than its tube capacity without refilling.
  */
 public class ExcessiveDeliveryException extends Throwable {
+
+    /**
+     * Used when specifying the number of items that are allowed to me delivered.
+     * @param maxItems is the max number of items this robot can deliver.
+     */
     public ExcessiveDeliveryException(int maxItems) {
         super(String.format("Attempting to deliver more than %d items in a single trip!!", maxItems));
     }
